3f89d1c5f4b03cb5c23ba05a16145012644c1799
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-06-15 17:57:14 -0700

    refactor: add new database structure

--- src/backend/db.ts
+++ src/backend/db.ts
@@ -1,7 +1,6 @@
 import { getCommonSchemaConfig } from 'multiverse/mongo-common';
 
-import type { ObjectId, WithId, WithoutId } from 'mongodb';
-import type { UnixEpochMs } from '@xunnamius/types';
+import type { ObjectId, WithId } from 'mongodb';
 import type { DbSchema } from 'multiverse/mongo-schema';
 
 /**
@@ -12,45 +11,29 @@ import type { DbSchema } from 'multiverse/mongo-schema';
 export function getSchemaConfig(): DbSchema {
   return getCommonSchemaConfig({
     databases: {
-      'hscc-api-drive': {
+      'hscc-slack': {
         collections: [
           {
-            name: 'users',
+            name: 'chapters',
             // ? Collation allows for case-insensitive searching. See:
             // ? https://stackoverflow.com/a/40914924/1367414
             createOptions: { collation: { locale: 'en', strength: 2 } },
             indices: [
-              { spec: 'key' },
+              { spec: 'name', options: { unique: true } },
+              { spec: 'administrators.primary' },
+              { spec: 'administrators.secondary' }
+            ]
+          },
+          {
+            name: 'apis',
+            // ? Collation allows for case-insensitive searching. See:
+            // ? https://stackoverflow.com/a/40914924/1367414
+            createOptions: { collation: { locale: 'en', strength: 2 } },
+            indices: [
               {
-                spec: 'username',
-                options: { unique: true }
-              },
-              {
-                spec: 'email',
+                spec: 'name',
                 options: { unique: true }
               }
-            ]
-          },
-          {
-            name: 'file-nodes',
-            indices: [
-              { spec: 'owner' },
-              { spec: 'name-lowercase' },
-              { spec: 'tags' },
-              // ? Wildcard indices to index permissions object keys
-              // ? https://www.mongodb.com/docs/manual/core/index-wildcard
-              { spec: 'permissions.$**' }
-            ]
-          },
-          {
-            name: 'meta-nodes',
-            indices: [
-              { spec: 'owner' },
-              { spec: 'name-lowercase' },
-              { spec: 'contents' },
-              // ? Wildcard indices to index permissions object keys
-              // ? https://www.mongodb.com/docs/manual/core/index-wildcard
-              { spec: 'permissions.$**' }
             ]
           }
         ]
@@ -61,238 +44,27 @@ export function getSchemaConfig(): DbSchema {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface UserId extends ObjectId {}
-export type Username = string;
+export interface PermissionId extends ObjectId {}
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface NodeId extends ObjectId {}
+export interface ApiId extends ObjectId {}
 
 /**
- * The shape of a filesystem node lock.
+ * The shape of an internal chapters entry.
  */
-export type NodeLock = {
-  user: Username;
-  client: string;
-  createdAt: UnixEpochMs;
-};
-
-/**
- * The shape of a filesystem node permission value.
- */
-export type NodePermission = 'view' | 'edit';
-
-/**
- * The shape of an internal filesystem file node.
- */
-export type InternalFileNode = WithId<{
-  type: 'file';
-  owner: Username;
-  createdAt: UnixEpochMs;
-  modifiedAt: UnixEpochMs;
+export type InternalChapter = WithId<{
   name: string;
-  'name-lowercase': string;
-  size: number;
-  text: string;
-  tags: string[];
-  lock: NodeLock | null;
-  permissions: Record<Username, NodePermission>;
+  administrators: {
+    primary: string[];
+    secondary: string[];
+  };
 }>;
 
 /**
- * The shape of a public filesystem file node.
+ * The shape of an internal apis entry.
  */
-export type PublicFileNode = WithoutId<Omit<InternalFileNode, 'name-lowercase'>> & {
-  node_id: string;
-};
-
-/**
- * The shape of a new filesystem file node.
- */
-export type NewFileNode = Partial<
-  Omit<
-    WithoutId<InternalFileNode>,
-    'owner' | 'createdAt' | 'modifiedAt' | 'name-lowercase' | 'size'
-  >
->;
-
-/**
- * The shape of a patch filesystem file node.
- */
-export type PatchFileNode = Partial<
-  Omit<
-    WithoutId<InternalFileNode>,
-    'type' | 'createdAt' | 'modifiedAt' | 'name-lowercase' | 'size'
-  >
->;
-
-/**
- * The shape of an internal filesystem meta node.
- */
-export type InternalMetaNode = WithId<{
-  type: 'directory' | 'symlink';
-  owner: Username;
-  createdAt: UnixEpochMs;
+export type InternalApi = WithId<{
   name: string;
-  'name-lowercase': string;
-  contents: NodeId[];
-  permissions: Record<Username, NodePermission>;
+  uri: string;
+  authVersion: 'legacy' | 'next-auth';
+  lastUnbanAt: number | null;
 }>;
-
-/**
- * The shape of a public filesystem meta node.
- */
-export type PublicMetaNode = Omit<
-  WithoutId<InternalMetaNode>,
-  'contents' | 'name-lowercase'
-> & {
-  node_id: string;
-  contents: string[];
-};
-
-/**
- * The shape of a new filesystem meta node.
- */
-export type NewMetaNode = Partial<
-  Omit<
-    WithoutId<InternalMetaNode>,
-    'owner' | 'createdAt' | 'name-lowercase' | 'contents'
-  > & {
-    contents: string[];
-  }
->;
-
-/**
- * The shape of a patch filesystem meta node.
- */
-export type PatchMetaNode = Partial<
-  Omit<
-    WithoutId<InternalMetaNode>,
-    'type' | 'createdAt' | 'name-lowercase' | 'contents'
-  > & { contents: string[] }
->;
-
-/**
- * The shape of an internal filesystem node.
- */
-export type InternalNode = InternalFileNode | InternalMetaNode;
-
-/**
- * The shape of a public filesystem node.
- */
-export type PublicNode = PublicFileNode | PublicMetaNode;
-
-/**
- * The shape of a new filesystem node.
- */
-export type NewNode = NewFileNode | NewMetaNode;
-
-/**
- * The shape of a patch filesystem node.
- */
-export type PatchNode = PatchFileNode | PatchMetaNode;
-
-/**
- * The shape of an internal application user.
- */
-export type InternalUser = WithId<{
-  username: Username;
-  salt: string;
-  email: string;
-  key: string;
-}>;
-
-/**
- * The shape of a public application user.
- */
-export type PublicUser = Omit<WithoutId<InternalUser>, 'key'> & { user_id: string };
-
-/**
- * The shape of a new application user.
- */
-export type NewUser = Partial<WithoutId<InternalUser>>;
-
-/**
- * The shape of a patch application user.
- */
-export type PatchUser = Partial<Omit<WithoutId<InternalUser>, 'username'>>;
-
-/**
- * Transforms an internal filesystem node into a public node.
- */
-export function toPublicNode(internalNode: InternalNode): PublicNode {
-  const publicNodeIntersection = {
-    node_id: internalNode._id.toString(),
-    type: internalNode.type,
-    owner: internalNode.owner,
-    createdAt: internalNode.createdAt,
-    name: internalNode.name,
-    permissions: internalNode.permissions
-  } as {
-    [key in keyof PublicFileNode & keyof PublicMetaNode]:
-      | PublicFileNode[key]
-      | PublicMetaNode[key];
-  };
-
-  if (internalNode.type == 'file') {
-    const publicNode = publicNodeIntersection as PublicFileNode;
-
-    publicNode.modifiedAt = internalNode.modifiedAt;
-    publicNode.size = internalNode.size;
-    publicNode.text = internalNode.text;
-    publicNode.tags = internalNode.tags;
-    publicNode.lock = internalNode.lock;
-
-    return publicNode;
-  } else {
-    const publicNode = publicNodeIntersection as PublicMetaNode;
-
-    publicNode.contents = internalNode.contents.map((id) => id.toString());
-
-    return publicNode;
-  }
-}
-
-/**
- * Transforms an internal user into a public user.
- */
-export function toPublicUser(internalUser: InternalUser): PublicUser {
-  return {
-    user_id: internalUser._id.toString(),
-    username: internalUser.username,
-    email: internalUser.email,
-    salt: internalUser.salt
-  };
-}
-
-export const publicUserProjection = {
-  _id: false,
-  user_id: { $toString: '$_id' },
-  username: true,
-  salt: true,
-  email: true
-} as const;
-
-export const publicFileNodeProjection = {
-  _id: false,
-  node_id: { $toString: '$_id' },
-  type: true,
-  owner: true,
-  createdAt: true,
-  modifiedAt: true,
-  name: true,
-  size: true,
-  text: true,
-  tags: true,
-  lock: true,
-  permissions: true
-} as const;
-
-export const publicMetaNodeProjection = {
-  _id: false,
-  node_id: { $toString: '$_id' },
-  type: true,
-  owner: true,
-  createdAt: true,
-  name: true,
-  contents: { $map: { input: '$contents', as: 'id', in: { $toString: '$$id' } } },
-  permissions: true
-} as const;
