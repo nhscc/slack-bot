41600fe277735026353777ff3081d80ca154500e
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-06-15 17:53:35 -0700

    refactor(env): add SLACK_SYSADMIN_USER_ID to .env.example

--- src/backend/env.ts
+++ src/backend/env.ts
@@ -11,7 +11,8 @@ export function getEnv<T extends Environment = Environment>() {
   const env = getDefaultEnv({
     SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID || '',
     SLACK_CLIENT_SECRET: process.env.SLACK_CLIENT_SECRET || '',
-    SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET || ''
+    SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET || '',
+    SLACK_SYSADMIN_USER_ID: process.env.SLACK_SYSADMIN_USER_ID || ''
   });
 
   // TODO: retire all of the following logic when expect-env is created. Also,
@@ -34,6 +35,10 @@ export function getEnv<T extends Environment = Environment>() {
 
     if (!env.SLACK_SIGNING_SECRET) {
       errors.push('bad SLACK_SIGNING_SECRET');
+    }
+
+    if (!env.SLACK_SYSADMIN_USER_ID) {
+      errors.push('bad SLACK_SYSADMIN_USER_ID');
     }
 
     // TODO: make it easier to reuse error code from getDefaultEnv. Or is it
