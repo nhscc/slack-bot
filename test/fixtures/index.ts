ef52f39663a3fa29d9423df233a9c6e81490e0db
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-06-15 18:08:15 -0700

    refactor(test): update fixtures to match backend stubs

--- test/fixtures/index.ts
+++ test/fixtures/index.ts
@@ -1,25 +1,44 @@
 import { asMockedFunction } from '@xunnamius/jest-types';
 
-import {
-  authAppUser,
-  createNode,
-  createUser,
-  deleteNodes,
-  deleteUser,
-  getAllUsers,
-  getNodes,
-  getUser,
-  searchNodes,
-  updateNode,
-  updateUser
-} from 'universe/backend';
+import {} from 'universe/backend';
 
-import EndpointCommand, {
-  config as ConfigCommand
-} from 'universe/pages/api/command/[command]';
+import EndpointCommandListApis, {
+  config as ConfigCommandListApis
+} from 'universe/pages/api/command/list-apis';
+
+import EndpointCommandGenApiKey, {
+  config as ConfigCommandGenApiKey
+} from 'universe/pages/api/command/gen-api-key';
+
+import EndpointCommandListApiKeys, {
+  config as ConfigCommandListApiKeys
+} from 'universe/pages/api/command/list-api-keys';
+
+import EndpointCommandDelApiKey, {
+  config as ConfigCommandDelApiKey
+} from 'universe/pages/api/command/del-api-key';
+
+import EndpointCommandUnbanApiKey, {
+  config as ConfigCommandUnbanApiKey
+} from 'universe/pages/api/command/unban-api-key';
+
+import EndpointCommandGrantPermissions, {
+  config as ConfigCommandGrantPermissions
+} from 'universe/pages/api/command/grant-permissions';
+
+import EndpointCommandRevokePermissions, {
+  config as ConfigCommandRevokePermissions
+} from 'universe/pages/api/command/revoke-permissions';
+
+import EndpointCommandListChapters, {
+  config as ConfigCommandListChapters
+} from 'universe/pages/api/command/list-chapters';
+
+import EndpointCommandCreateChapter, {
+  config as ConfigCommandCreateChapter
+} from 'universe/pages/api/command/create-chapter';
 
 import type { NextApiHandler, PageConfig } from 'next';
-import type { PublicNode, PublicUser } from 'universe/backend/db';
 
 export type NextApiHandlerMixin = NextApiHandler & {
   config?: PageConfig;
@@ -27,13 +46,45 @@ export type NextApiHandlerMixin = NextApiHandler & {
 };
 
 /**
+ * Dummy Slack client ID.
+ */
+export const SLACK_CLIENT_ID = '123456789123.1234567891234';
+
+/**
+ * Dummy Slack client secret.
+ */
+export const SLACK_CLIENT_SECRET = '12345678912345678912345678912345';
+
+/**
+ * Dummy Slack signing secret taken from
+ * https://api.slack.com/authentication/verifying-requests-from-slack#sdk_support
+ */
+export const SLACK_SIGNING_SECRET = '8f742231b10e8888abcd99yyyzzz85a5';
+
+/**
  * The entire live API topology gathered together into one convenient object.
  */
 export const api = {
-  command: EndpointCommand as NextApiHandlerMixin
+  commandListApis: EndpointCommandListApis as NextApiHandlerMixin,
+  commandGenApiKey: EndpointCommandGenApiKey as NextApiHandlerMixin,
+  commandListApiKeys: EndpointCommandListApiKeys as NextApiHandlerMixin,
+  commandDelApiKey: EndpointCommandDelApiKey as NextApiHandlerMixin,
+  commandUnbanApiKey: EndpointCommandUnbanApiKey as NextApiHandlerMixin,
+  commandGrantPermissions: EndpointCommandGrantPermissions as NextApiHandlerMixin,
+  commandRevokePermissions: EndpointCommandRevokePermissions as NextApiHandlerMixin,
+  commandListChapters: EndpointCommandListChapters as NextApiHandlerMixin,
+  commandCreateChapter: EndpointCommandCreateChapter as NextApiHandlerMixin
 };
 
-api.command.config = ConfigCommand;
+api.commandListApis.config = ConfigCommandListApis;
+api.commandGenApiKey.config = ConfigCommandGenApiKey;
+api.commandListApiKeys.config = ConfigCommandListApiKeys;
+api.commandDelApiKey.config = ConfigCommandDelApiKey;
+api.commandUnbanApiKey.config = ConfigCommandUnbanApiKey;
+api.commandGrantPermissions.config = ConfigCommandGrantPermissions;
+api.commandRevokePermissions.config = ConfigCommandRevokePermissions;
+api.commandListChapters.config = ConfigCommandListChapters;
+api.commandCreateChapter.config = ConfigCommandCreateChapter;
 
 /**
  * A convenience function that mocks the entire backend and returns the mock
@@ -43,43 +94,12 @@ api.command.config = ConfigCommand;
  * function!**
  */
 export function setupMockBackend() {
-  const mockedAuthAppUser = asMockedFunction(authAppUser);
-  const mockedCreateNode = asMockedFunction(createNode);
-  const mockedCreateUser = asMockedFunction(createUser);
-  const mockedDeleteNodes = asMockedFunction(deleteNodes);
-  const mockedDeleteUser = asMockedFunction(deleteUser);
-  const mockedGetAllUsers = asMockedFunction(getAllUsers);
-  const mockedGetNodes = asMockedFunction(getNodes);
-  const mockedGetUser = asMockedFunction(getUser);
-  const mockedSearchNodes = asMockedFunction(searchNodes);
-  const mockedUpdateNode = asMockedFunction(updateNode);
-  const mockedUpdateUser = asMockedFunction(updateUser);
-
-  beforeEach(() => {
-    mockedAuthAppUser.mockReturnValue(Promise.resolve(false));
-    mockedCreateNode.mockReturnValue(Promise.resolve({} as PublicNode));
-    mockedCreateUser.mockReturnValue(Promise.resolve({} as PublicUser));
-    mockedDeleteNodes.mockReturnValue(Promise.resolve());
-    mockedDeleteUser.mockReturnValue(Promise.resolve());
-    mockedGetAllUsers.mockReturnValue(Promise.resolve([]));
-    mockedGetNodes.mockReturnValue(Promise.resolve([]));
-    mockedGetUser.mockReturnValue(Promise.resolve({} as PublicUser));
-    mockedSearchNodes.mockReturnValue(Promise.resolve([]));
-    mockedUpdateNode.mockReturnValue(Promise.resolve());
-    mockedUpdateUser.mockReturnValue(Promise.resolve());
-  });
-
-  return {
-    mockedAuthAppUser,
-    mockedCreateNode,
-    mockedCreateUser,
-    mockedDeleteNodes,
-    mockedDeleteUser,
-    mockedGetAllUsers,
-    mockedGetNodes,
-    mockedGetUser,
-    mockedSearchNodes,
-    mockedUpdateNode,
-    mockedUpdateUser
-  };
+  // TODO
+  // const mockedAuthAppUser = asMockedFunction(authAppUser);
+  // beforeEach(() => {
+  //   mockedAuthAppUser.mockReturnValue(Promise.resolve(false));
+  // });
+  // return {
+  //   mockedAuthAppUser,
+  // };
 }
